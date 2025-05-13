a086fa5480fc09259b5f1d8d9029a7c0ad1b2163
Qinghaima <lorenzohansy@gmail.com>
2025-05-13 17:04:15 +0800

    Add files via upload

---
+++ assets/firebase-init.js
@@ -0,0 +1,18 @@
+<!-- 引入 Firebase SDK 和 Auth 模块 -->
+<script src="https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyBC262GHqTJxk61mLAB6sIH9KFD6NfMtLQ",
+    authDomain: "rinka-site-5a986.firebaseapp.com",
+    projectId: "rinka-site-5a986",
+    storageBucket: "rinka-site-5a986.appspot.com",
+    messagingSenderId: "85571120840",
+    appId: "1:85571120840:web:0d7602aefb667f814f3ffd",
+    measurementId: "G-C81RQBJECD"
+  };
+
+  // 初始化 Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
