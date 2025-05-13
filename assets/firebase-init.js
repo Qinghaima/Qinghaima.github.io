c073b35c017636a5ca7e6418f8135a04a041c054
Qinghaima <lorenzohansy@gmail.com>
2025-05-13 17:18:50 +0800

    Update firebase-init.js

--- assets/firebase-init.js
+++ assets/firebase-init.js
@@ -1,18 +1,16 @@
-<!-- 引入 Firebase SDK 和 Auth 模块 -->
-<script src="https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js"></script>
+// 引入 Firebase 模块（这部分保留在 HTML 里）
+// 这部分内容不要放进 firebase-init.js
+// <script src="https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js"></script>
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyBC262GHqTJxk61mLAB6sIH9KFD6NfMtLQ",
-    authDomain: "rinka-site-5a986.firebaseapp.com",
-    projectId: "rinka-site-5a986",
-    storageBucket: "rinka-site-5a986.appspot.com",
-    messagingSenderId: "85571120840",
-    appId: "1:85571120840:web:0d7602aefb667f814f3ffd",
-    measurementId: "G-C81RQBJECD"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBC262GHqTJxk61mLAB6sIH9KFD6NfMtLQ",
+  authDomain: "rinka-site-5a986.firebaseapp.com",
+  projectId: "rinka-site-5a986",
+  storageBucket: "rinka-site-5a986.appspot.com",
+  messagingSenderId: "85571120840",
+  appId: "1:85571120840:web:0d7602aefb667f814f3ffd",
+  measurementId: "G-C81RQBJECD"
+};
 
-  // 初始化 Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+firebase.initializeApp(firebaseConfig);
